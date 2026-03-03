bef7a45ef38b0d5d90a419cb3e659dc6cf28b629
higorxi <higorgiovane7@gmail.com>
2026-03-02 21:53:56 -0300

    Integrate Prisma with PostgreSQL adapter and update package dependencies

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from '../generated/prisma/client';
+
+
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL!,
+});
+export const prisma = new PrismaClient({ adapter });
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHello() {
+
+    console.log({ prisma });
+
+    return 'test';
   }
 }
