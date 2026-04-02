3525be6d396d40cbe6b0819626876d4806a6f98a
higorxi <higorgiovane7@gmail.com>
2026-04-01 21:45:42 -0300

    Add Auth, Users, Companies, and Schedules modules with controllers, services, and DTOs; include tests for each module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { CompaniesModule } from './companies/companies.module';
+import { SpacesModule } from './spaces/spaces.module';
+import { SchedulesModule } from './schedules/schedules.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UsersModule, CompaniesModule, SpacesModule, SchedulesModule],
   controllers: [AppController],
   providers: [AppService],
 })
