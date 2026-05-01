9604f3105ed73e36419946b4c7dbe50b97a75c7b
higorxi <higorgiovane7@gmail.com>
2026-05-01 16:11:26 -0300

    fix: update CORS origin to allow access from localhost:5173

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
   app.enableCors({
-    origin: ['http://localhost:5174'],
+    origin: ['http://localhost:5173', ], // 'quadra-connect-admin.vercel.app'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
